9906f4a5438c4e23efe4f834af91a9c7d5f38608
unknown <aelise17264@gmail.com>
2021-06-07 13:02:20 -0600

    finished user schema

--- models/user.js
+++ models/user.js
@@ -3,11 +3,12 @@ const Schema = mongoose.Schema;
 
 //define our schema modle
 const userSchema = new Schema({
-    email: String,
+    email: {type: String, unique: true}, //to avoid repeats of the same email address
     password: String
 });
 
 //create the model class
-
+const ModelClass = mongoose.model('user', userSchema);
 
 //export the model so the app can use it
+module.exports = ModelClass;
