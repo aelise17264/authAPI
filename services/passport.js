e183f40cd5ab5bbeec3e54343856b51773eb9288
unknown <aelise17264@gmail.com>
2021-06-08 16:11:04 -0600

    set up first passport strategy

--- services/passport.js
+++ services/passport.js
@@ -4,3 +4,27 @@ const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 
+//set up options for JWT strategy
+const jwtOptions = {
+
+}
+
+//Create JWT strategy
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done){
+//see if the new user ID in the payload exists in our db
+//if it does call 'done' w/ that other
+//otherwise call 'done' w/o a user object
+    User.findById(payload.sub, function(err, user){
+        if(err){
+            return done(err, false); //this user is not authenticated
+        }
+
+        if(user){
+            done(null, user);
+        }else{
+            done(null, false);//search failed
+        }
+    })
+})
+
+// Tell passport to use this strategy
