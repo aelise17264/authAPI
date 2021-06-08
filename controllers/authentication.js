2feede50c68e109557faaa5d2cf86c9e89f46e5e
unknown <aelise17264@gmail.com>
2021-06-08 12:30:15 -0600

    post request to signup new users working

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,3 +1,35 @@
+const User = require('../models/user');
+const mongoose = require('mongoose')
+
+mongoose.set('useCreateIndex', true)
+
 exports.signup = function(req, res, next){
-    res.send({success: 'true'});
+    const email = req.body.email;
+    const password= req.body.password;
+    //see if a user w/ a given email exists
+    User.findOne({email: email}, function(error, existingUser){
+        if(error){
+            return next(error)
+        }
+    //if a user w/ this email does exists, return an error
+        if(existingUser){
+            return res.status(422).send({error: 'this email already exists'})//unprocessible request
+        }
+        
+    //if a user w/ email is new, create & save user record
+        const user = new User({
+            email: email,
+            password: password
+        })
+        user.save(function(error){
+            if(error){
+                return next(error)
+            }
+            res.json(user)
+        })
+
+    //respond to request indicating the user was created
+
+    })
+
 }
