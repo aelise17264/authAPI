70ea9b0f4a5d0aac1b85e2156fbb85f62a0f4abe
unknown <aelise17264@gmail.com>
2021-06-09 14:15:37 -0600

    added cors to server so that frontend app can make requests to backend

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 const router = require('./router');
@@ -10,9 +11,11 @@ const router = require('./router');
 //App setup
 app.use(morgan('combined')); //logging framework
 app.use(bodyParser.json({type: '*/*'})); //any incoming request is converted into JSON
+app.use(cors());
 //these are our express middlewares
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static("public"))
+
 
 //db setup
 mongoose.connect("mongodb://localhost:27017/auth", {useNewUrlParser: true, useUnifiedTopology: true}); //creates a new database in mongodb called auth
@@ -22,7 +25,7 @@ router(app)
 
 
 // Server setup
-const port = process.envPORT || 3000;
+const port = process.envPORT || 4000;
 const server = http.createServer(app);
 server.listen(port);
 console.log('Server running on', port);
